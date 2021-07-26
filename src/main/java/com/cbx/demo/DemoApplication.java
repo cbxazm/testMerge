c058f94f19521c585ead413865b488430f3059f9
jackson cui <50820823+cbxazm@users.noreply.github.com>
2021-07-26 17:06:48 +0800

    Update DemoApplication.java

--- src/main/java/com/cbx/demo/DemoApplication.java
+++ src/main/java/com/cbx/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("ahead native");
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("master add");
+		System.out.println("masterhahahaha");
 		System.out.println("ahead native");
 
 
